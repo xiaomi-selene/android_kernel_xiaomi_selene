b73e2bb191bcc0c14964678c639463d8b7c21cb2
fukiame <fukiame@proton.me>
2024-09-18 11:26:57 +0700

    drivers/misc: mediatek: video: dispsys: kill DSI_DumpRegisters
    Signed-off-by: fukiame <fukiame@proton.me>

--- drivers/misc/mediatek/video/mt6768/dispsys/ddp_dsi.c
+++ drivers/misc/mediatek/video/mt6768/dispsys/ddp_dsi.c
@@ -308,6 +308,7 @@ static const char *_dsi_vdo_mode_parse_state(unsigned int state)
 
 enum DSI_STATUS DSI_DumpRegisters(enum DISP_MODULE_ENUM module, int level)
 {
+#if 0
 	u32 i = 0;
 	u32 k = 0;
 
@@ -390,7 +391,7 @@ enum DSI_STATUS DSI_DumpRegisters(enum DISP_MODULE_ENUM module, int level)
 #endif
 		}
 	}
-
+#endif
 	return DSI_STATUS_OK;
 }
 
