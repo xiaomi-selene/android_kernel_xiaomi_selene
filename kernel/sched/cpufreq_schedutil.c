2c70370d19d1b72554a647d958a936dd03d54af1
fukiame <fukiame@proton.me>
2025-02-23 15:20:00 +0700

    Revert "[ALPS04247928] sched/uclamp: enahance uclamp trace"
    This reverts commit 7208e5c43f779c5d32adae5c8ddc20f8823565cd.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -423,7 +423,7 @@ static unsigned int sugov_next_freq_shared(struct sugov_cpu *sg_cpu, u64 time)
 		j_max = j_sg_cpu->max;
 
 #ifdef CONFIG_UCLAMP_TASK
-		trace_schedutil_uclamp_util(j, j_util);
+		trace_uclamp_util_dvfs(j, j_util);
 #endif
 
 		j_util = uclamp_util(cpu_rq(j), j_util);
