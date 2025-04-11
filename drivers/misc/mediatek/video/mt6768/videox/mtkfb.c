a3e5c1cd18d3c61e5b67b4fb79a4b6d74401e471
erabye <erabye@tuta.io>
2025-04-11 17:14:06 +0700

    Revert "[ALPS06383763] disp: fix ioctl fuzzer ke"
    This reverts commit c9fee7548d92cb5a9ebbeedcbdd1001d1fc9ef0d.
    
    Signed-off-by: erabye <erabye@tuta.io>

--- drivers/misc/mediatek/video/mt6768/videox/mtkfb.c
+++ drivers/misc/mediatek/video/mt6768/videox/mtkfb.c
@@ -1292,7 +1292,7 @@ unsigned int mtkfb_fm_auto_test(void)
 	}
 
 	if (idle_state_backup) {
-		primary_display_idlemgr_kick(__func__, 1);
+		primary_display_idlemgr_kick(__func__, 0);
 		disp_helper_set_option(DISP_OPT_IDLEMGR_ENTER_ULPS, 0);
 	}
 	fbVirAddr = (unsigned long)fbdev->fb_va_base;
@@ -1326,8 +1326,7 @@ unsigned int mtkfb_fm_auto_test(void)
 
 	mtkfb_pan_display_impl(&mtkfb_fbi->var, mtkfb_fbi);
 	msleep(100);
-
-	primary_display_idlemgr_kick(__func__, 1);
+	primary_display_idlemgr_kick(__func__, 0);
 	result = primary_display_lcm_ATA();
 
 	if (idle_state_backup)
